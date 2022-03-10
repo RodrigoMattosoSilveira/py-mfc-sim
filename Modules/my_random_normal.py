763e68ad2dd5581d44d19c050165b59d5df30b0f
Rodrigo Silveira <rodrigo.silveira@ohi.com>
2022-03-09 18:52:55 -0800

    0001-get-order - Wrote the outline

---
+++ Modules/my_random_normal.py
@@ -0,0 +1,10 @@
+import numpy as np
+import random
+from math import trunc
+
+
+def get_random_normal(mu, sigma):
+    s = np.random.normal(mu, sigma, 100)
+    i = random.randint(0, 99)
+    s = np.random.normal(mu, sigma, 100)
+    return trunc(s[i] + 0.5)
