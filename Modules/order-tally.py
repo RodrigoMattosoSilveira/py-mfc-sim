abe6db10a8ac44d4050c6f389f48cf12822a32b8
Rodrigo Silveira <rodrigo.silveira@ohi.com>
2022-03-11 05:58:23 -0800

    0003-trigger-simulation-end Added class to track a order fulfillment data

---
+++ Modules/order-tally.py
@@ -0,0 +1,19 @@
+import secrets
+import string
+
+
+class OrderTally(object):
+    """
+    Used to collect order fulfillment data
+    """
+    def __init__(self, ppp_id):
+        self.pppId = ppp_id
+        self.orderId = ''.join(secrets.choice(string.ascii_uppercase + string.digits) for i in range(8))
+        self.orderTime = 0
+        self.pickTime = 0
+        self.packTime = 0
+        self.labelTime = 0
+        self.deliveryTime = 0
+        self.readyTime = 0
+        self.workTime = 0
+        self.items = 0
