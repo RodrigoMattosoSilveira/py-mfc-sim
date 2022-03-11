0645cfae206e1f3749fd653e40379f1e048dabee
Rodrigo Silveira <rodrigo.silveira@ohi.com>
2022-03-11 08:38:53 -0800

    0003-trigger-simulation-end Added setters and getters

--- Modules/order-tally.py
+++ Modules/order-tally.py
@@ -5,15 +5,96 @@ import string
 class OrderTally(object):
     """
     Used to collect order fulfillment data
+    See https://www.tutorialsteacher.com/python/property-decorator for info on property decorators
     """
     def __init__(self, ppp_id):
-        self.pppId = ppp_id
-        self.orderId = ''.join(secrets.choice(string.ascii_uppercase + string.digits) for _ in range(8))
-        self.orderTime = 0
-        self.pickTime = 0
-        self.packTime = 0
-        self.labelTime = 0
-        self.deliveryTime = 0
-        self.readyTime = 0
-        self.workTime = 0
-        self.items = 0
+        self.__pppId = ppp_id
+        self.__orderId = ''.join(secrets.choice(string.ascii_uppercase + string.digits) for _ in range(8))
+        self.__orderTime = 0
+        self.__pickTime = 0
+        self.__packTime = 0
+        self.__labelTime = 0
+        self.__deliveryTime = 0
+        self.__courierTime = 0
+        self.__workTime = 0
+        self.__items = 0
+
+    @property
+    def pppId(self):
+        return self.__pppId
+
+    @pppId.setter
+    def pppId(self, value):
+        self.__pppId = value
+
+    @property
+    def orderId(self):
+        return self.__orderId
+
+    @orderId.setter
+    def orderId(self, value):
+        self.__orderId = value
+
+    @property
+    def orderTime(self):
+        return self.__orderTime
+
+    @orderTime.setter
+    def orderTime(self, value):
+        self.__orderTime = value
+
+    @property
+    def pickTime(self):
+        return self.__pickTime
+
+    @pickTime.setter
+    def pickTime(self, value):
+        self.__pickTime = value
+
+    @property
+    def packTime(self):
+        return self.__packTime
+
+    @packTime.setter
+    def packTime(self, value):
+        self.__packTime = value
+
+    @property
+    def labelTime(self):
+        return self.__labelTime
+
+    @labelTime.setter
+    def labelTime(self, value):
+        self.__labelTime = value
+
+    @property
+    def deliveryTime(self):
+        return self.__deliveryTime
+
+    @deliveryTime.setter
+    def deliveryTime(self, value):
+        self.__deliveryTime = value
+
+    @property
+    def courierTime(self):
+        return self.__courierTime
+
+    @courierTime.setter
+    def courierTime(self, value):
+        self.__courierTime = value
+
+    @property
+    def workTime(self):
+        return self.__workTime
+
+    @workTime.setter
+    def workTime(self, value):
+        self.__workTime = value
+
+    @property
+    def items(self):
+        return self.__items
+
+    @items.setter
+    def items(self, value):
+        self.__items = value
--- Modules/ppp-daily-tally.py
+++ Modules/ppp-daily-tally.py
@@ -6,12 +6,70 @@ import parameters as params
 class PppDailyTally(object):
     """
     Used to collect PPP shift data
+    See https://www.tutorialsteacher.com/python/property-decorator for info on property decorators
     """
+
     def __init__(self):
-        self.pppId = ''.join(secrets.choice(string.ascii_uppercase + string.digits) for _ in range(8))
-        self.orders = 0
-        self.workTime = 0
-        self.hourBreakDuration = 0
-        self.shiftBreakDuration = 0
-        self.nextHourBreak = 3600 - params.HOUR_BREAK_DURATION
-        self.nextShiftBreak = params.SHIFT_WORK_DURATION/params.SHIFT_BREAKS_PER_SHIFT
+        self.__pppId = ''.join(secrets.choice(string.ascii_uppercase + string.digits) for _ in range(8))
+        self.__orders = 0
+        self.__workTime = 0
+        self.__hourBreakDuration = 0
+        self.__shiftBreakDuration = 0
+        self.__nextHourBreak = 3600 - params.HOUR_BREAK_DURATION
+        self.__nextShiftBreak = params.SHIFT_WORK_DURATION / params.SHIFT_BREAKS_PER_SHIFT
+
+    @property
+    def pppId(self):
+        return self.__pppId
+
+    @pppId.setter
+    def pppId(self, value):
+        self.__pppId = value
+
+    @property
+    def orders(self):
+        return self.__orders
+
+    @orders.setter
+    def orders(self, value):
+        self.__orders = value
+
+    @property
+    def workTime(self):
+        return self.__workTime
+
+    @workTime.setter
+    def workTime(self, value):
+        self.__workTime = value
+
+    @property
+    def hourBreakDuration(self):
+        return self.__hourBreakDuration
+
+    @hourBreakDuration.setter
+    def hourBreakDuration(self, value):
+        self.__hourBreakDuration = value
+
+    @property
+    def shiftBreakDuration(self):
+        return self.__shiftBreakDuration
+
+    @shiftBreakDuration.setter
+    def shiftBreakDuration(self, value):
+        self.__shiftBreakDuration = value
+
+    @property
+    def nextHourBreak(self):
+        return self.__nextHourBreak
+
+    @nextHourBreak.setter
+    def nextHourBreak(self, value):
+        self.__nextHourBreak = value
+
+    @property
+    def nextShiftBreak(self):
+        return self.__nextShiftBreak
+
+    @nextShiftBreak.setter
+    def nextShiftBreak(self, value):
+        self.__nextShiftBreak = value
