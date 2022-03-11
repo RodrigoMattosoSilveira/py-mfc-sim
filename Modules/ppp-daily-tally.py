750115f97c218522c9edc93c622d341a7550efba
Rodrigo Silveira <rodrigo.silveira@ohi.com>
2022-03-11 05:54:19 -0800

    0003-trigger-simulation-end Added class to track a ppp's daily work

---
+++ Modules/ppp-daily-tally.py
@@ -0,0 +1,17 @@
+import secrets
+import string
+import parameters as params
+
+
+class PppDailyTally(object):
+    """
+    Used to collect PPP shift data
+    """
+    def __init__(self):
+        self.pppId = ''.join(secrets.choice(string.ascii_uppercase + string.digits) for i in range(8))
+        self.orders = 0
+        self.workTime = 0
+        self.hourBreakDuration = 0
+        self.shiftBreakDuration = 0
+        self.nextHourBreak = 3600 - params.HOUR_BREAK_DURATION
+        self.nextShiftBreak = params.SHIFT_WORK_DURATION/params.SHIFT_BREAKS_PER_SHIFT
