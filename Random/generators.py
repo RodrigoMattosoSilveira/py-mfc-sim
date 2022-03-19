4f4aaf537c565d55c966c1c4c7053e4af2f37374
Rodrigo Silveira <rodrigo.silveira@ohi.com>
2022-03-18 17:22:18 -0700

    0020_missing_kit_item_sku Added amethod to generate a Random Number from an Arbitrary Probability Distribution

--- Random/generators.py
+++ Random/generators.py
@@ -25,3 +25,36 @@ def get_random_poisson(a):
     return random_number
 
 
+# Random Number Generator Arbitrary Probability Distribution
+# Utility function to find ceiling of r in arr[length..h]
+def find_ceil(arr, r, length, h):
+    while length < h:
+        mid = length + ((h - length) >> 1)  # Same as mid = (length+h)/2
+        if r > arr[mid]:
+            length = mid + 1
+        else:
+            h = mid
+
+    if arr[length] >= r:
+        return length
+    else:
+        return -1
+
+
+# Random Number Generator Arbitrary Probability Distribution
+def rng_apd(arr, freq, n):
+
+    # Create and fill prefix array
+    prefix = [0] * n
+    prefix[0] = freq[0]
+    for i in range(n):
+        prefix[i] = prefix[i - 1] + freq[i]
+
+    # prefix[n-1] is sum of all frequencies.
+    # Generate a random number with
+    # value from 1 to this sum
+    r = random.randint(0, prefix[n - 1]) + 1
+
+    # Find index of ceiling of r in prefix array
+    index_c = find_ceil(prefix, r, 0, n - 1)
+    return arr[index_c]
